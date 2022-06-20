4f9371b06bb6988cd9613cf6a2995559031652dc
blacksnail789521 <blacksnail789521@gmail.com>
2022-06-20 12:17:57 +0800

    add TAG and remove PORT

--- PyTorch/README.md
+++ PyTorch/README.md
@@ -1,4 +1,4 @@
 1. Go to [NGC release note](https://docs.nvidia.com/deeplearning/frameworks/tensorflow-release-notes/index.html) to see which version of image we want to pull, and type that version into `.env`
 2. Use `docker-compose build` to create image
 3. Use `docker-compose run` to run a container based on the service on the Dockerfile
-   - Just use the script `create_container.sh`
+   - Just use the script `create_container.sh` (remember to alter `TAG`)
--- PyTorch/create_container.sh
+++ PyTorch/create_container.sh
@@ -1,3 +1,15 @@
 source .env
-tag=${NGC_IMAGE_VERSION//./_} # replace "." to "_"
-docker-compose run --service-ports --name jason_pt_${tag} jason-pt
+
+TAG=$1
+if ((${#TAG} > 1)) ; then
+	echo "len(TAG) > 1"
+    return
+fi
+
+tag=${NGC_IMAGE_VERSION//./_}_${TAG} # replace "." to "_"
+port=${tag//_/}
+echo tag: ${tag}
+echo port: ${port}
+
+#docker-compose run --service-ports --name jason_pt_${tag} jason-pt
+docker-compose run -p ${port}:22 --name jason_pt_${tag} jason-pt
